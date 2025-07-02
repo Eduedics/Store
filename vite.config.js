84fe0a5bd2f80f2c62f264e7d477cd37ffe32940
Eduedics <karaniedward2000@gmail.com>
2025-07-02 17:34:13 +0300

    root path to database change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8800',
+        target: 'http://localhost:800',
         changeOrigin: true,
         secure: false,
       },
